a9082b2cc14eda8898cb7bf2ceadc7f338e233f2
Jae Lo Presti [j4] <git@777.tf>
2024-12-29 18:41:50 +0200

    feat: setup base mod

---
+++ BlockX/Blocker.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+
+namespace BlockX
+{
+    public class Blocker : ResoniteMod
+    {
+        public override string Name => "BlockX";
+        public override string Author => "Jae \"J4\" Lo Presti";
+        public override string Version => "0.1.0";
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<string> blockListUrl = new ModConfigurationKey<string>("blockListUrl", "The default URL to fetch a blocklist from.", () => "https://i.j4.lc/resonite/bl.txt");
+
+        private static ModConfiguration Config;
+        
+        public override void OnEngineInit()
+        {
+            Config = GetConfiguration();
+
+            Harmony harmony = new Harmony("lc.j4.blockx");
+            harmony.PatchAll();
+            
+            Msg("Insert buckazoid.");
+        }
+    }
+}
--- BlockX/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BlockX
-{
-    public class Class1
-    {
-    }
-}
